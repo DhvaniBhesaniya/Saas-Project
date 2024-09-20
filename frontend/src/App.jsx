34bd44f9189eddc16fe17908605d36f074d59fab
DhvaniBhesaniya <mailto:narutogokuluffy009@gmail.com>
2024-09-20 17:56:30 +0530

    added login and blog code.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MarketingPage from "./marketing-page/MarketingPage";
+import SignInSide from "./sign-in-side/SignInSide";
+import Blog from "./marketing-page/Blog";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/home" element={<MarketingPage />} />
+        <Route path="/login" element={<SignInSide />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
